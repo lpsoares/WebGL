b4ee6fb35126e4bbd27ad7da3186e272ec31a20b
RogerPina2 <roger.pina12@hotmail.com>
2020-10-21 17:06:36 -0300

    Proj2-part1

--- triangulo.js
+++ triangulo.js
@@ -4,10 +4,11 @@ var points;
 window.onload = function init()
 {
     var canvas = document.getElementById( "gl-canvas" );
+    
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
+    var vertices = new Float32Array([-1, -1, 1, -1, 1, 1);
 
     //  Configura WebGL
     gl.viewport( 0, 0, canvas.width, canvas.height );
@@ -34,6 +35,6 @@ window.onload = function init()
 function render() 
 {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+    gl.drawArrays( gl.TRIANGLES, 0, 3);
 }
 
